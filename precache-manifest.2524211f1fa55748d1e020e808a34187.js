abbe4cd8e4fdf23f1951a12f0233afc12da230fd
Evgeny Malischev <malischev.e.s@gmail.com>
2022-02-21 00:49:22 +0300

    change dist

---
+++ precache-manifest.2524211f1fa55748d1e020e808a34187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d87a77f05c84fd42eac",
+    "url": "/todolist/css/app.0c280459.css"
+  },
+  {
+    "revision": "efbafe9e47ccbc1078b1d30d12ce0965",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "5d87a77f05c84fd42eac",
+    "url": "/todolist/js/app.57258373.js"
+  },
+  {
+    "revision": "c2954931b719e98dd47b",
+    "url": "/todolist/js/chunk-vendors.ec27726f.js"
+  },
+  {
+    "revision": "6ab4156a6f3ce6c1e0da26bacc034d07",
+    "url": "/todolist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todolist/robots.txt"
+  }
+]);
